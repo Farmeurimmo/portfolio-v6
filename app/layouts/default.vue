e964609eae24ee933aa1fed685fb691c5b3080d1
Farmeurimmo <contact@farmeurimmo.fr>
2026-09-24 21:41:37 +0200

    feat: WIP on default layout and update app structure

---
+++ app/layouts/default.vue
@@ -0,0 +1,17 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <nav class="">
+
+  </nav>
+  <div>
+    <p>Some default layout content shared across all pages</p>
+    <slot />
+  </div>
+</template>
+
+<style scoped>
+
+</style>
